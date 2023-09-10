996ea617264aa2517e1ceba3d2ee3f2e914e3ba0
Naffer Reyes <reyesnaffer@gmail.com>
2023-09-10 17:58:01 -0500

    custom filter for go

--- 01/remove.go
+++ 01/remove.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -9,6 +10,18 @@ func is_even(number int) (r bool) {
 	r = number%2 == 0
 	return
 }
+
+func customFilter(list []int, fun func(number int) bool) (results []int) {
+	for _, val := range list {
+		if fun(val) {
+			results = append(results, val)
+		}
+	}
+
+	return
+}
+
+// No built-in function for Go?
 
 func ExecuteTests(t *testing.T) {
 	if !is_even(2) {
@@ -21,16 +34,22 @@ func ExecuteTests(t *testing.T) {
 		t.Errorf("is_even(0) = false; want true")
 	}
 
+	testNumbers := []int{1, 2, 3, 5, 6, 4, 7, 8, 9, 22, 12, 10}
+	rightResponse := []int{2, 6, 4, 8, 22, 12, 10}
+
+	if !reflect.DeepEqual(customFilter(testNumbers, is_even), rightResponse) {
+		t.Errorf("customFilter(%v) = rightResponse; want true", testNumbers)
+	}
+
 	fmt.Println("Tests passed")
 }
 
 func main() {
+	numbers := []int{1, 2, 3, 5, 6, 4, 7, 8, 9, 22, 12, 10}
 
-	numbers := [...]int{1, 2, 3, 5, 6, 4, 7, 8, 9, 22, 12, 10}
+	evenNumbersFromCustom := customFilter(numbers, is_even)
 
-	// evenNumbersFromBuiltIn := Choose(numbers, is_even)
-
-	fmt.Printf("From built-in: %v\n", numbers)
+	fmt.Printf("From custom: %v\n", evenNumbersFromCustom)
 
 	t := new(testing.T)
 	ExecuteTests(t)
